6e089d0f45c0875465d548c9d5fa397efb7af984
leanvallejos <leanvallejos90@gmail.com>
2021-05-13 15:33:41 -0300

    Correccion de tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -7,6 +7,7 @@ import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
@@ -20,42 +21,41 @@ public class MonederoTest {
   @Test
   void Poner() {
     cuenta.poner(1500);
-  } //falta validar el test
+    assertEquals(cuenta.getSaldo(), 1500);
+  }
 
   @Test
   void PonerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
-  } //creo que deberia testear el mensaje de la exception
+  }
 
   @Test
   void TresDepositos() {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
-  } //no valida nada que los movimientos tengan 3 depositos
+    assertEquals(cuenta.getMovimientos().size() , 3);
+  }
 
   @Test
-  void MasDeTresDepositos() { //el nombre podria estar mejor, como "LimiteDeDepositosEn
-    assertThrows(MaximaCantidadDepositosException.class, () -> {
-          cuenta.poner(1500);
-          cuenta.poner(456);
-          cuenta.poner(1900);
-          cuenta.poner(245);
-    }); //
+  void LimiteDeDepositosDiario() {
+    cuenta.poner(1500);
+    cuenta.poner(456);
+    cuenta.poner(1900);
+    assertThrows(MaximaCantidadDepositosException.class, () -> {cuenta.poner(245);});
   }
 
   @Test
   void ExtraerMasQueElSaldo() {
     assertThrows(SaldoMenorException.class, () -> {
-          cuenta.setSaldo(90); //esta linea esta de mas
           cuenta.sacar(1001);
-    }); //puede ser que debamos validar el mensaje de la excepcion
+    });
   }
 
   @Test
-  public void ExtraerMasDe1000() { //este test esta de mas, no aporta
+  public void ExtraerMasDelLimite() { //Si aportaba pero estaba con mal nombre, eso me confundio
     assertThrows(MaximoExtraccionDiarioException.class, () -> {
-      cuenta.setSaldo(5000); //esta linea esta de mas
+      cuenta.setSaldo(5000);
       cuenta.sacar(1001);
     });
   }
@@ -63,6 +63,6 @@ public class MonederoTest {
   @Test
   public void ExtraerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
-  } //esta excepcion no corresponde, deberia haber una que sea ExtraccionNegativaException
+  }
 
 }
